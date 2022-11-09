8129a9b06d668e8bcd172cd06fa4ba892212b308
Anthony <alasam@live.com>
2022-11-09 02:28:13 -0500

    create initial navbar and home pages, add video for home background, house cleaning

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="fixed z-50 w-full h-[80px] flex justify-between items-center px-4 bg-black text-white"></div>
+  );
+}
+
+export default Navbar;
