fb5e9667f4f890ff1b5b8e0049f283968d1b1277
Anthony <alasam@live.com>
2022-11-10 14:36:57 -0500

    animated banner adjustments, navbar adjustments

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { GiBasketballBasket } from "react-icons/gi";
 import court from "../assets/court.jpg";
 import highlight from "../assets/highlight.mp4";
 
@@ -7,6 +8,14 @@ function Banner() {
     <div className="w-full h-screen bg-black pt-[80px]">
       {" "}
       <video src={highlight} autoPlay loop muted />
+      <div className="content">
+        <h1 className="text-white text-8xl underline decoration-blue-500 py-1 flex flex-nowrap">
+          <GiBasketballBasket size="150px" />
+          INBOUNDS MINISTRIES
+        </h1>
+        <p className="text-white text-4xl">Young Adults Basketball Ministry</p>
+        <p className="text-white text-4xl">Bramalea Baptist Church</p>
+      </div>
     </div>
   );
 }
