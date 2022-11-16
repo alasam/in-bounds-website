f42e8fc53730f83f90d8268a652b8f145c7205d3
Anthony <alasam@live.com>
2022-11-16 02:25:16 -0500

    install env dependancy, setup gitignore

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Banner from "./components/Banner";
+import Calendar from "./components/Calendar";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Banner />
       <Home />
+      <Calendar />
     </div>
   );
 }
